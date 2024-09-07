d26521f7a6fb9672f68d06fa2fddef60d0ee8559
Muhammad <muhannmmad@gmail.com>
2024-09-07 14:00:42 +0200

    menue coded

---
+++ main.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+void main() {
+  bool isProgramRunning = true;
+
+  while (isProgramRunning) {
+    print('Login(L)/ SignUp(S)/ Exit(E)');
+
+    //  user input
+    String? userChoice = stdin.readLineSync();
+
+    switch (userChoice) {
+      case 'L':
+        // Login
+        stdout.write('Enter username: ');
+        String? username = stdin.readLineSync();
+        stdout.write('Enter password: ');
+        String? password = stdin.readLineSync();
+        print('Logging in with username: $username and password: $password');
+        break;
+
+      case 'S':
+        // SignUp
+        stdout.write('Enter new username: ');
+        String? newUsername = stdin.readLineSync();
+        stdout.write('Enter new password: ');
+        String? newPassword = stdin.readLineSync();
+        print(
+            'Signing up with username: $newUsername and password: $newPassword');
+        // Add sign-up logic and validation here
+        break;
+
+      case 'E':
+        // Exit
+        isProgramRunning = false;
+        print('Exiting program. Goodbye!');
+        break;
+
+      default:
+        print('Invalid choice. Please select L, S, P, or E.');
+    }
+  }
+}
